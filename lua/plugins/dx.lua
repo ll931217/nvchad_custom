4908272e1a5b6b4c1cc77920779d8131ffadbc4d
Liang-Shih Lin <liangshihlin@gmail.com>
2024-03-11 05:24:45 +0800

    Use another zellij nav plugin

--- lua/plugins/dx.lua
+++ lua/plugins/dx.lua
@@ -81,14 +81,11 @@ return {
   },
 
   {
-    "https://git.sr.ht/~swaits/zellij-nav.nvim",
-    lazy = true,
-    event = "VeryLazy",
-    keys = {
-      { "<C-h>", "<cmd>ZellijNavigateLeft<CR>", { silent = true, desc = "Navigate Left" } },
-      { "<C-j>", "<cmd>ZellijNavigateDown<CR>", { silent = true, desc = "Navigate Down" } },
-      { "<C-k>", "<cmd>ZellijNavigateUp<CR>", { silent = true, desc = "Navigate Up" } },
-      { "<C-l>", "<cmd>ZellijNavigateRight<CR>", { silent = true, desc = "Navigate Right" } },
-    }
-  }
+    "Lilja/zellij.nvim",
+    config = function()
+      require("zellij").setup({
+        vimTmuxNavigatorKeybinds = true,
+      })
+    end
+  },
 }
